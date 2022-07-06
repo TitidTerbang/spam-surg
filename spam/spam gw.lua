4c153b361fd6f81d275901b1acc8d6fd43f4d496
TitidTerbang <yudisbaek@gmail.com>
2022-07-07 01:40:09 +0700

    mengubah sleep menghapus for i, action exit, sleep, dan joinworld

--- spam/spam gw.lua
+++ spam/spam gw.lua
@@ -8,7 +8,7 @@ kordinatY = 47  --45
 
 function JoinWorld()
     sendPacket(3,"action|join_request\nname|"..world)
-    sleep(5000)
+    sleep(4000)
 end
 
 function ccek()
@@ -20,18 +20,13 @@ function ccek()
 end
 
 function spam()
+    findPath(kordinatX-1, kordinatY-1)  
     while getBot().world == world do
-        findPath(kordinatX-1, kordinatY-1)  
         sleep(1000)
-        for i=25,1,-1 do
-                say(text1)
-                sleep(DELAY)
-                say(text2)
-                sleep(DELAY)
-        end
-        sendPacket(3, "action|quit_to_exit")
-        sleep(60000)
-        JoinWorld()
+        say(text1)
+        sleep(DELAY)
+        say(text2)
+        sleep(DELAY)
     end
 end
 
